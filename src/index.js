59896ca33faeebc869737fc1c354c0855be7a36b
guruvesh-garg <ggguruveshgarg@gmail.com>
2022-05-13 13:41:49 +0530

    Routing navigation updated but not working properly

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Register from './MyComponents/Register';
 import reportWebVitals from './reportWebVitals';
-//import Login from './MyComponents/Login';
-//import Guest from './MyComponents/Guest';
+import Login from './MyComponents/Login';
+import Guest from './MyComponents/Guest';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App/>}/>
+        <Route exact path="/Register" element={<Register/>}/>
+        <Route exact path="/Login" element={<Login/>}/>
+        <Route exact path="/Guest" element={<Guest/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
